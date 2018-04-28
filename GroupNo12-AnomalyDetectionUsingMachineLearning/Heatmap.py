147fb2af6db3523cd0620089e4d63e79ef3c7059
Vedant Yadav <yadavvedant@gmail.com>
2018-04-28 15:13:27 +0530

    Update Heatmap.py

--- GroupNo12-AnomalyDetectionUsingMachineLearning/Heatmap.py
+++ GroupNo12-AnomalyDetectionUsingMachineLearning/Heatmap.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import catheat
-df=pd.read_csv('C:/Users/papibono/Desktop/BE_PROJECT/1521029641_1296.csv',header=0)
+df=pd.read_csv('1521029641_1296.csv',header=0)
 # Plot the categorical columns as heatmap
 ax = catheat.heatmap( df[['SourceName','TaskCategory','EventCode','EventType']],
                       palette='Paired' )
